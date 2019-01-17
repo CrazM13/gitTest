18852ab4d7c6d10b65d59178d32c6e25956cd750
Sepulveda <jasepulveda@labs.neit.edu>
2019-01-17 12:17:36 -0500

    Started Server

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-console.log("this is our test for github");
-console.log("this is our test for github");
+const http = require('http');
+const timeStamp = require('./timeStamp');
+
+http.createServer(function(req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<p>The Date And Time Is <b>" + timeStamp.timeStamp() + "</b></p><br />");
+    res.end("<p>Welcome to the <b><i>Homepage</i></b>!</p>");
+}).listen(3030);
